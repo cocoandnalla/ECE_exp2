4ef30d81956d509e8f0307704a054e3e351a3d48
cocoandnalla <144300103+cocoandnalla@users.noreply.github.com>
2023-09-18 09:56:15 +0900

    last

--- week3/comparator/4bitcomparator/4bitcomparator.runs/impl_1/comparator.tcl
+++ week3/comparator/4bitcomparator/4bitcomparator.runs/impl_1/comparator.tcl
@@ -115,6 +115,8 @@ proc step_failed { step } {
 OPTRACE "impl_1" END { }
 }
 
+set_msg_config -id {Synth 8-256} -limit 10000
+set_msg_config -id {Synth 8-638} -limit 10000
 
 OPTRACE "impl_1" START { ROLLUP_1 }
 OPTRACE "Phase: Write Bitstream" START { ROLLUP_AUTO }
@@ -123,7 +125,9 @@ start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
+  set_param checkpoint.writeSynthRtdsInDcp 1
   set_param chipscope.maxJobs 2
+  set_param synth.incrementalSynthesisCache C:/Users/82106/Documents/GitHub/ECE_exp2/week3/comparator/4bitcomparator/.Xil/Vivado-14972-BAYERNchampions/incrSyn
   set_param runs.launchOptions { -jobs 4  }
   open_checkpoint comparator_routed.dcp
   set_property webtalk.parent_dir C:/Users/82106/Documents/GitHub/ECE_exp2/week3/comparator/4bitcomparator/4bitcomparator.cache/wt [current_project]
