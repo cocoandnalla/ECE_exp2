92a428a0d9dbe67b37a3a2c91a5e3ec921de2b91
cocoandnalla <144300103+cocoandnalla@users.noreply.github.com>
2023-09-18 09:47:47 +0900

    week3

--- week3/mux4to1/mux4/mux4.runs/impl_1/rundef.js
+++ week3/mux4to1/mux4/mux4.runs/impl_1/rundef.js
@@ -24,7 +24,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "init_design", "begin" );
+ISETouchFile( "write_bitstream", "begin" );
 ISEStep( "vivado",
          "-log mux4to1.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source mux4to1.tcl -notrace" );
 
