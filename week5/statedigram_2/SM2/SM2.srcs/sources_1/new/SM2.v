c33a95fc922231b5b85180399c69e4972df02bb9
cocoandnalla <144300103+cocoandnalla@users.noreply.github.com>
2023-10-27 22:39:20 +0900

    SM1

---
+++ week5/statedigram_2/SM2/SM2.srcs/sources_1/new/SM2.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/10/27 22:39:04
+// Design Name: 
+// Module Name: SM2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SM2(
+
+    );
+endmodule
